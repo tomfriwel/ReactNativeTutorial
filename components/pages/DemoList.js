584b0e11eaa4b63519a94f2abaf1e5c0178665d3
tomfriwel <976932447@qq.com>
2018-04-02 14:50:21 +0800

    m

---
+++ components/pages/DemoList.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+export default class extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Demo List</Text>
+            </View>
+        )
+    }
+}
