a53855778257f263a2667036f37dc3d9815c18ce
tomfriwel <976932447@qq.com>
2018-04-08 11:45:34 +0800

    add

--- pages/WithNavigationDemo.js
+++ pages/WithNavigationDemo.js
@@ -2,17 +2,24 @@
 
 import React from 'react';
 import { View, Button } from 'react-native';
-import { withNavigation } from 'react-navigation';
+import { StackNavigator, withNavigation } from 'react-navigation';
 
 class MyBackButton extends React.Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                 <Button title="Back" onPress={() => { this.props.navigation.goBack() }} />;
+                <Button title="Push" onPress={() => { this.props.navigation.navigate('Home') }} />;
             </View>
         )
     }
 }
+
+const stack = StackNavigator({
+    Home: {
+        screen: MyBackButton
+    }
+})
 
 // withNavigation returns a component that wraps MyBackButton and passes in the
 // navigation prop
