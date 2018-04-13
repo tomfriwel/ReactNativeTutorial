535c29b59efcaa2f99198e8ce91fbbc0fe21c9f0
tomfriwel <976932447@qq.com>
2018-04-13 09:36:21 +0800

    add

---
+++ ios/BluetoothManager/BluetoothManager.h
@@ -0,0 +1,13 @@
+//
+//  BluetoothManager.h
+//  ReactNativeTutorial
+//
+//  Created by tomfriwel on 2018/4/13.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BluetoothManager : NSObject
+
+@end
---
+++ ios/BluetoothManager/BluetoothManager.m
@@ -0,0 +1,13 @@
+//
+//  BluetoothManager.m
+//  ReactNativeTutorial
+//
+//  Created by tomfriwel on 2018/4/13.
+//  Copyright © 2018 Facebook. All rights reserved.
+//
+
+#import "BluetoothManager.h"
+
+@implementation BluetoothManager
+
+@end
